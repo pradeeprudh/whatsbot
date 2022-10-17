0313567819e3b2d1e255d0f3fc68aae6ac5331c5
pradeeprudh <116023183+pradeeprudh@users.noreply.github.com>
2022-10-17 21:27:39 +0300

    Update sticker.js

--- modules/sticker.js
+++ modules/sticker.js
@@ -1,16 +1,4 @@
-/* ═══════════════════════════════════════════════════════ //
-=> If you want to recode, reupload,
-=> or copy the codes/script,
-=> pls give credit,
-=> no credit? i will take action immediately.
-==> Copyright (C) 2022 Dark_Ezio.
-==> Licensed under the  MIT License;
-===> you may not use this file except in compliance with the License.
-=> Thank you to Lord Buddha, Family and Myself.
-=> Whats Bot - Dark_Ezio.
-// ════════════════════════════ */
-
-const { delay } = require("Wa-Web");
+const { delay } = require('@adiwajshing/baileys')
 const ezio = require("../events");
 const { isUrl } = require("../lib/Function");
 const lang = ezio.getString("whats_bot");
