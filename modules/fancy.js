03713c97b00f87c3f3af9c3ec82827e23d69d46c
pradeeprudh <116023183+pradeeprudh@users.noreply.github.com>
2022-10-18 12:26:44 +0300

    Update fancy.js

--- modules/fancy.js
+++ modules/fancy.js
@@ -1,20 +1,13 @@
-
 const { command, styletext, listall, tiny, isPrivate } = require("../lib/");
-/* Copyright (C) 2022 X-Electra.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-X-Asena - X-Electra
-*/
-
-command(
+ezio.addCommand(
   {
     pattern: "fancy",
     fromMe: isPrivate,
     desc: "converts text to fancy text",
     type: "converter",
   },
-  async (message, match) => {
-    if (!message.reply_message || !message.reply_message.text || !match ||isNaN(match)) {
+  async (message, client) => {
+    if (!message.reply_message || !message.reply_message.text || !client ||isNaN(client)) {
       let text = tiny(
         "Fancy text generator\n\nReply to a message\nExample: .fancy 32\n\n"
       );
@@ -23,7 +16,7 @@ command(
       });
       return await message.reply(text);
     } else {
-      message.reply(styletext(message.reply_message.text, parseInt(match)));
+      message.reply(styletext(message.reply_message.text, parseInt(client)));
     }
   }
 );
